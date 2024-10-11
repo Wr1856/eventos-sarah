dc6d512834438a3411c2ac32d77d4c3b07a81273
Elivelton Santos <samsepi0l.dev@gmail.com>
2024-10-11 10:44:14 -0300

    chore: delete folder dist

--- apps/server/dist/main/factories/cryptography/bcrypt-adapt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeBcryptAdapter = void 0;
-const bcrypt_adapt_1 = require("../../../infra/gateways/bcrypt-adapt");
-const makeBcryptAdapter = () => {
-    const salt = 12;
-    return new bcrypt_adapt_1.BcryptAdapt(salt);
-};
-exports.makeBcryptAdapter = makeBcryptAdapter;
--- apps/server/dist/main/factories/repositories/user-account.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makePgUserAccountRepository = void 0;
-const user_repository_1 = require("../../../infra/repos/postgres/user-repository");
-const makePgUserAccountRepository = () => {
-    return new user_repository_1.PgUserAccountRepository();
-};
-exports.makePgUserAccountRepository = makePgUserAccountRepository;
