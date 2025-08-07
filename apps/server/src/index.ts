2b19b46fdacd15a0cfd7a78492b8070e419a24b9
Weessmr <89693081+Wr1856@users.noreply.github.com>
2025-08-07 17:49:45 -0300

    Handle server startup errors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -33,5 +33,10 @@ registerEventRoutes(app);
 const PORT = env.PORT || 3000;
 
 app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
-  console.log(`🚀 server listening on ${address}`);
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+
+  app.log.info(`🚀 server listening on ${address}`);
 });
